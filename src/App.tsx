b0d2f664a5b3c2ef1fe1bef43a4afb0fd56efcaf
Htetko510217 <htetko510217@gmai.com>
2024-06-02 01:35:38 +0700

    init : project layout setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from './components/user-quiz-result/Card'
+function App() {
+
+  return (
+    <>
+      <h1>Quiz</h1>
+      <Card />
+    </>
+  )
+}
+
+export default App
